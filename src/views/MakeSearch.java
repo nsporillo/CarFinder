ea38785630a05d8d352772c832c7a62eeaa0b6a9
Nick Porillo <nsporillo@gmail.com>
2018-03-20 20:04:48 -0400

    Update views to close just that view on exit

--- src/views/MakeSearch.java
+++ src/views/MakeSearch.java
@@ -29,7 +29,7 @@ public class MakeSearch extends JFrame {
 	 */
 	public MakeSearch() {
 		setTitle("Search By Make");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 800, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
--- src/views/MakeView.java
+++ src/views/MakeView.java
@@ -15,7 +15,7 @@ public class MakeView extends JFrame {
 	 * Create the frame.
 	 */
 	public MakeView() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
