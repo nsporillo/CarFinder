453df1d8b57992765d888d71b98d7309e6213005
n1nj4vn <n1nj4vn@users.noreply.github.com>
2018-03-21 22:11:25 -0400

    updated javadoc

--- src/tables/DealerInventoryTable.java
+++ src/tables/DealerInventoryTable.java
@@ -5,8 +5,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DealerInventoryTable {
+
   /**
    * Adds a single vehicle to the database
+   *
+   * @param conn
+   * @param vin
+   * @param dealerID
    */
   public static void addVehicle(Connection conn, int vin, int dealerID) {
 
