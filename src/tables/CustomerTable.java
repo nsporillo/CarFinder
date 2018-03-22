3abc9754727cefd53e5bb79e3113909e5d222dc4
n1nj4vn <n1nj4vn@users.noreply.github.com>
2018-03-21 21:16:12 -0400

    updated CustomerTable for addCustomer functionality

--- src/tables/CustomerTable.java
+++ src/tables/CustomerTable.java
@@ -1,5 +1,30 @@
 package tables;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CustomerTable {
+
+  /**
+   * Adds a single customer to the database
+   */
+  public static void addCustomer(Connection conn, int customerID, String firstName, String lastName,
+      String street, String city, int zip, String state, int phone, int gender, int income) {
+
+    String query = String.format("INSERT INTO Customer "
+            + "VALUES(%d,\'%s\',\'%s\',\'%s\',\'%s\',\'%d\'\'%s\'\'%d\'\'%d\'\'%d\');", customerID,
+        firstName, lastName, state, city, zip, state, phone, gender, income);
+
+    try {
+      /**
+       * create and execute the query
+       */
+      Statement stmt = conn.createStatement();
+      stmt.execute(query);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
 
 }
