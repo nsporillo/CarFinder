2e81f3ef3b36beaf35d8a90c5b2abf3e4a1c16ed
n1nj4vn <n1nj4vn@users.noreply.github.com>
2018-03-21 22:17:37 -0400

    updated ModelTable for addModel functionality

--- src/tables/ModelTable.java
+++ src/tables/ModelTable.java
@@ -1,5 +1,33 @@
 package tables;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class ModelTable {
+
+  /**
+   * Adds a single model to the database
+   *
+   * @param conn
+   * @param modelID
+   * @param brandName
+   * @param bodyStyle
+   */
+  public static void addModel(Connection conn, int modelID, String brandName, String bodyStyle) {
+
+    String query = String.format("INSERT INTO Customer "
+            + "VALUES(%d,\'%s\',\'%s\');", modelID, brandName, bodyStyle);
+
+    try {
+      /**
+       * create and execute the query
+       */
+      Statement stmt = conn.createStatement();
+      stmt.execute(query);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
 
 }
