fa5f1a1d8db3e5b677b277cf365fd4621bb2fde6
n1nj4vn <n1nj4vn@users.noreply.github.com>
2018-03-21 22:29:13 -0400

    updated SaleTable for addSale functionality

--- src/tables/SaleTable.java
+++ src/tables/SaleTable.java
@@ -1,5 +1,36 @@
 package tables;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class SaleTable {
+
+  /**
+   * Adds a single sale to the database
+   *
+   * @param conn
+   * @param saleID
+   * @param dealerID
+   * @param customerID
+   * @param timestamp
+   * @param vin
+   */
+  public static void addSale(Connection conn, int saleID, int dealerID, int customerID, Date timestamp, int vin) {
+
+    String query = String.format("INSERT INTO Sale "
+            + "VALUES(%d,\'%d\',\'%d\',\'%tD\',\'%d\');", saleID, dealerID, customerID, timestamp, vin);
+
+    try {
+      /**
+       * create and execute the query
+       */
+      Statement stmt = conn.createStatement();
+      stmt.execute(query);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
 
 }
