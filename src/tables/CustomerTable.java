870156ccbde1ca8975699afc48d4eb84157bf37b
n1nj4vn <n1nj4vn@users.noreply.github.com>
2018-03-21 22:08:41 -0400

    corrected addCustomer query

--- src/tables/CustomerTable.java
+++ src/tables/CustomerTable.java
@@ -8,13 +8,25 @@ public class CustomerTable {
 
   /**
    * Adds a single customer to the database
+   *
+   * @param conn
+   * @param customerID
+   * @param firstName
+   * @param lastName
+   * @param street
+   * @param city
+   * @param zip
+   * @param state
+   * @param phone
+   * @param gender
+   * @param income
    */
   public static void addCustomer(Connection conn, int customerID, String firstName, String lastName,
       String street, String city, int zip, String state, int phone, int gender, int income) {
 
     String query = String.format("INSERT INTO Customer "
             + "VALUES(%d,\'%s\',\'%s\',\'%s\',\'%s\',\'%d\'\'%s\'\'%d\'\'%d\'\'%d\');", customerID,
-        firstName, lastName, state, city, zip, state, phone, gender, income);
+        firstName, lastName, street, city, zip, state, phone, gender, income);
 
     try {
       /**
