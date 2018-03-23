1a0c997915ca6be637350036b59ced033bbd5aff
n1nj4vn <n1nj4vn@users.noreply.github.com>
2018-03-22 22:07:04 -0400

    added queryOptionTable

--- src/tables/OptionTable.java
+++ src/tables/OptionTable.java
@@ -90,6 +90,85 @@ public class OptionTable {
     return sb.toString();
   }
 
+  /**
+   * Makes a query to the option table
+   * with given columns and conditions
+   *
+   * @param conn
+   * @param columns: columns to return
+   * @param whereClauses: conditions to limit query by
+   * @return
+   */
+  public static ResultSet queryOptionTable(Connection conn,
+      ArrayList<String> columns,
+      ArrayList<String> whereClauses){
+    StringBuilder sb = new StringBuilder();
+
+    /**
+     * Start the select query
+     */
+    sb.append("SELECT ");
+
+    /**
+     * If we gave no columns just give them all to us
+     *
+     * other wise add the columns to the query
+     * adding a comma top seperate
+     */
+    if(columns.isEmpty()){
+      sb.append("* ");
+    }
+    else{
+      for(int i = 0; i < columns.size(); i++){
+        if(i != columns.size() - 1){
+          sb.append(columns.get(i) + ", ");
+        }
+        else{
+          sb.append(columns.get(i) + " ");
+        }
+      }
+    }
+
+    /**
+     * Tells it which table to get the data from
+     */
+    sb.append("FROM person ");
+
+    /**
+     * If we gave it conditions append them
+     * place an AND between them
+     */
+    if(!whereClauses.isEmpty()){
+      sb.append("WHERE ");
+      for(int i = 0; i < whereClauses.size(); i++){
+        if(i != whereClauses.size() -1){
+          sb.append(whereClauses.get(i) + " AND ");
+        }
+        else{
+          sb.append(whereClauses.get(i));
+        }
+      }
+    }
+
+    /**
+     * close with semi-colon
+     */
+    sb.append(";");
+
+    //Print it out to verify it made it right
+    System.out.println("Query: " + sb.toString());
+    try {
+      /**
+       * Execute the query and return the result set
+       */
+      Statement stmt = conn.createStatement();
+      return stmt.executeQuery(sb.toString());
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
   public static void printOptionTable(Connection conn) {
 
     String query = "SELECT * FROM Option;";
