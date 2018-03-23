283a479aff91811bf3e2e3c0271ebbaa18f89371
Nick Porillo <nsporillo@gmail.com>
2018-03-23 18:44:09 -0400

    Add some dealer table queries

--- src/models/Dealer.java
+++ src/models/Dealer.java
@@ -2,25 +2,67 @@ package models;
 
 public class Dealer {
 
-	private String name, street, state, phone;
+	private String name, street, city, state, phone;
 	private int zip, id;
 
-	public Dealer(String name, int id, String Street, String State, int zip, String phone) {
-		super();
+	public Dealer() {}
+	
+	public Dealer(String name, int id, String street, String city, String state, int zip, String phone) {
 		this.name = name;
 		this.id = id;
-		this.street = Street;
-		this.state = State;
+		this.street = street;
+		this.city = city;
+		this.state = state;
 		this.zip = zip;
 		this.phone = phone;
 	}
-
+	
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
 	}
 
 	public int getId() {
@@ -31,14 +73,6 @@ public class Dealer {
 		this.id = id;
 	}
 
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	
 	public String getAddress() {
 		return street + ", " + state + ", " + zip;
 	}
--- src/tables/DealerTable.java
+++ src/tables/DealerTable.java
@@ -7,12 +7,17 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class DealerTable {
+	
+	private static final String table = "Dealer";
 
   public static void populateDealerTableFromCSV(Connection conn, String filename) throws SQLException{
     ArrayList<Dealer> dealers = new ArrayList<>();
@@ -22,9 +27,16 @@ public class DealerTable {
       int dealerID = 0;
       while((line = br.readLine()) != null){
         String[] split = line.split(",");
-        dealers.add(new Dealer(nameGen(split[0]), dealerID, split[1],
-                split[2], Integer.parseInt(split[3]), split[4]));
-        dealerID ++;
+        
+        Dealer dealer = new Dealer();
+        dealer.setName(nameGen(split[0]));
+        dealer.setId(dealerID++);
+        dealer.setStreet(split[1]);
+        //TODO: Set city
+        dealer.setState(split[2]);
+        dealer.setZip(Integer.parseInt(split[3]));
+        dealer.setPhone(split[4]);
+        dealers.add(dealer);
       }
       br.close();
     } catch (FileNotFoundException e) {
@@ -33,6 +45,7 @@ public class DealerTable {
       e.printStackTrace();
     }
   }
+  
   private static String nameGen(String name){
     Random r = new Random();
     int coolified = r.nextInt() % 5;
@@ -56,6 +69,7 @@ public class DealerTable {
     return "errored";
   }
 
+
   /**
    * Adds a single dealer to the database
    *
@@ -71,7 +85,7 @@ public class DealerTable {
   public static void addDealer(Connection conn, int dealerID, String name,
       String street, String city, int zip, String state, int phone) {
 
-    String query = String.format("INSERT INTO Dealer "
+    String query = String.format("INSERT INTO " + table + " "
             + "VALUES(%d,\'%s\',\'%s\',\'%s\',\'%d\',\'%s\',\'%d\');", dealerID,
         name, state, city, zip, state, phone);
 
@@ -86,4 +100,74 @@ public class DealerTable {
     }
   }
 
+	/**
+	 * Finds a dealer given a dealerId
+	 * @param conn
+	 * @param dealerId
+	 * @return dealer object 
+	 */
+	public static Dealer getById(Connection conn, int dealerId) {
+		PreparedStatement ps = null;
+		String query = "SELECT * FROM " + table + " WHERE ID = ?";
+
+		try {
+			ps = conn.prepareStatement(query);
+			ps.setInt(1, dealerId);
+			ResultSet rs = ps.executeQuery();
+			Dealer dealer = new Dealer();
+
+			while (rs.next()) {
+				dealer.setId(rs.getInt(0));
+				dealer.setName(rs.getString(1));
+				dealer.setState(rs.getString(2));
+				dealer.setCity(rs.getString(3));
+				dealer.setZip(rs.getInt(rs.getInt(4)));
+				dealer.setState(rs.getString(5));
+				dealer.setPhone(rs.getString(6));
+			}
+			
+			ps.close();
+			return dealer;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		} 
+	}
+	
+	/**
+	 * Filters dealers on a given name
+	 * @param conn
+	 * @param name
+	 * @return list of dealer objects
+	 */
+	public static List<Dealer> filterDealersByName(Connection conn, String name) {
+		List<Dealer> dealers = new ArrayList<>();
+		PreparedStatement ps = null;
+		String query = "SELECT * FROM " + table + " WHERE name LIKE %?%";
+
+		try {
+			ps = conn.prepareStatement(query);
+			ps.setString(1, name);
+			ResultSet rs = ps.executeQuery();
+
+			Dealer dealer = new Dealer();
+
+			while (rs.next()) {
+				dealer.setId(rs.getInt(0));
+				dealer.setName(rs.getString(1));
+				dealer.setState(rs.getString(2));
+				dealer.setCity(rs.getString(3));
+				dealer.setZip(rs.getInt(rs.getInt(4)));
+				dealer.setState(rs.getString(5));
+				dealer.setPhone(rs.getString(6));
+				dealers.add(dealer);
+			}
+
+			ps.close();
+			return dealers;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
