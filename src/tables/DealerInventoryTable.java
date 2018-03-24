c073e581a7e81a5eccb55a18a088c88fe7bfad29
n1nj4vn <n1nj4vn@users.noreply.github.com>
2018-03-24 13:03:22 -0400

    adding DealerInventory createDealerInventorySQL functionality

--- src/tables/DealerInventoryTable.java
+++ src/tables/DealerInventoryTable.java
@@ -1,9 +1,13 @@
 package tables;
 
+import models.Customer;
+import models.DealerInventory;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class DealerInventoryTable {
 
@@ -28,6 +32,24 @@ public class DealerInventoryTable {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public static String createDealerInventoryInsertSQL(ArrayList<DealerInventory> dealerInv) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("INSERT INTO DealerInventory (VIN, DealerID) VALUES");
+
+        for (int i = 0; i < dealerInv.size(); i++) {
+            DealerInventory di = dealerInv.get(i);
+            sb.append(String.format("(%d,\'%d\')", di.getVin(), di.getDealerID()));
+            if (i != dealerInv.size() - 1) {
+                sb.append(",");
+            } else {
+                sb.append(";");
+            }
+        }
+
+        return sb.toString();
     }
 
     public static void printDealerInventoryTable(Connection conn) {
