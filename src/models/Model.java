8f2dea5ee0ffd949221f130bc653b5dc1581a706
DavidIsraelChambers <dic9883@rit.edu>
2018-03-24 11:44:32 -0400

    Added Model class and funtionality to populate ModelTable

---
+++ src/models/Model.java
@@ -0,0 +1,40 @@
+package models;
+
+public class Model {
+
+    private int id;
+    private String brandName;
+    private String bodyStyle;
+
+    public Model(int modelID, String brandName, String bodystyle) {
+        this.id = modelID;
+        this.brandName = brandName;
+        this.bodyStyle = bodystyle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getBodyStyle() {
+        return bodyStyle;
+    }
+
+    public void setBodyStyle(String bodyStyle) {
+        this.bodyStyle = bodyStyle;
+    }
+
+
+}
--- src/tables/ModelTable.java
+++ src/tables/ModelTable.java
@@ -1,10 +1,48 @@
 package tables;
 
+import models.Model;
+import models.Option;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class ModelTable {
+
+  /**
+   * Reads a cvs file for data and adds them to the model table
+   *
+   * Does not create the table. It must already be created
+   *
+   * @param conn     database connection to work with
+   * @param fileName fileName of CSV file containing model table data
+   * @throws SQLException
+   */
+  public static void populateOptionTableFromCSV(Connection conn, String fileName) throws SQLException {
+    ArrayList<Model> model = new ArrayList<>();
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(fileName));
+      String line;
+      while((line = br.readLine()) != null){
+        String[] split = line.split(",");
+        model.add(new Model(Integer.parseInt(split[0]), split[1], split[2]));
+      }
+      br.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (NumberFormatException e) {
+      e.printStackTrace();
+    }
+
+    String sql = createModelInsertSQL(model);
+
+    Statement stmt = conn.createStatement();
+    stmt.execute(sql);
+  }
 
   /**
    * Adds a single model to the database
@@ -30,4 +68,23 @@ public class ModelTable {
     }
   }
 
+  public static String createModelInsertSQL(ArrayList<Model> model) {
+    StringBuilder sb = new StringBuilder();
+
+    sb.append("INSERT INTO Model (ModelID, BrandName, BodyStyle) VALUES");
+
+    for(int i = 0; i < model.size(); i++){
+      Model m = model.get(i);
+      sb.append(String.format("(%d,\'%s\',\'%s\')",
+              m.getId(), m.getBrandName(), m.getBodyStyle()));
+      if( i != model.size()-1){
+        sb.append(",");
+      }
+      else{
+        sb.append(";");
+      }
+    }
+
+    return sb.toString();
+  }
 }
