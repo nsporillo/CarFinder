1e9e8fe09b483e3b52421999013d0614885cf92a
n1nj4vn <n1nj4vn@users.noreply.github.com>
2018-03-24 12:55:43 -0400

    adding Print functionality

--- src/tables/DealerInventoryTable.java
+++ src/tables/DealerInventoryTable.java
@@ -1,32 +1,50 @@
 package tables;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DealerInventoryTable {
 
-  /**
-   * Adds a single vehicle to the database
-   *
-   * @param conn
-   * @param vin
-   * @param dealerID
-   */
-  public static void addVehicle(Connection conn, int vin, int dealerID) {
+    /**
+     * Adds a single vehicle to the database
+     *
+     * @param conn
+     * @param vin
+     * @param dealerID
+     */
+    public static void addVehicle(Connection conn, int vin, int dealerID) {
 
-    String query = String.format("INSERT INTO DealerInventory "
-            + "VALUES(%d,\'%d\');", vin, dealerID);
+        String query = String.format("INSERT INTO DealerInventory "
+                + "VALUES(%d,\'%d\');", vin, dealerID);
 
-    try {
-      /**
-       * create and execute the query
-       */
-      Statement stmt = conn.createStatement();
-      stmt.execute(query);
-    } catch (SQLException e) {
-      e.printStackTrace();
+        try {
+            /**
+             * create and execute the query
+             */
+            Statement stmt = conn.createStatement();
+            stmt.execute(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-  }
+
+    public static void printDealerInventoryTable(Connection conn) {
+
+        String query = "SELECT * FROM DealerInventory;";
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet result = stmt.executeQuery(query);
+
+            while (result.next()) {
+                System.out.printf("DealerInventory %d: %d\n",
+                        result.getInt(1),
+                        result.getInt(2));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
