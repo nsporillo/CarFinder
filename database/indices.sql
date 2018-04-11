2e6c8d4b63cae6d5fd712e6b3821533c209d889b
Nick Porillo <nsporillo@gmail.com>
2018-04-10 22:40:20 -0400

    Revamp table creation script
    - Improves formatting for readability
    - Adds indices
    - Adds foreign keys constraints
    - Adds not null constraints
    - Adds comments

--- database/indices.sql
+++
@@ -1,4 +0,0 @@
--- Adds an index on the DealerID column in DealerInventory for faster selection
-CREATE INDEX IF NOT EXISTS DealerIndex ON DealerInventory(DealerID);
--- Add index for selecting all sales for a dealer 
-CREATE INDEX IF NOT EXISTS SaleIndex ON Sale(DealerID)
