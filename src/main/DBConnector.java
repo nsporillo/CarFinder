bace8bb915d8556fda13d717d73736e97ae075ef
n1nj4vn <n1nj4vn@users.noreply.github.com>
2018-04-12 13:48:08 -0400

    Added functionality for TODO - just have to parse file and set variables if we want the functionality

--- src/main/DBConnector.java
+++ src/main/DBConnector.java
@@ -17,96 +17,98 @@ import java.util.List;
 
 public class DBConnector {
 
-	private static final File dbFile = new File("database/SQLTables.sql.mv.db");
-	private static final File dbPath = new File("database/SQLTables.sql");
+    private static final File dbFile = new File("database/SQLTables.sql.mv.db");
+    private static final File dbPath = new File("database/SQLTables.sql");
 
-	// The connection to the database
-	private Connection conn;
+    // The connection to the database
+    private Connection conn;
 
-	public DBConnector() {
-		if (dbFile.exists()) {
-			createConnection(dbPath.getAbsolutePath());
-			Team01Driver.log("Database exists: Skipping table creation and population");
-		} else {
-			Team01Driver.log("Existing database not found: Creating one and loading data from CSV.");
-			createDatabase(dbPath.getAbsolutePath());
-			populateDatabase();
-		}
+    public DBConnector(String username, String password) {
+        if (dbFile.exists()) {
+            createConnection(dbPath.getAbsolutePath(), username, password);
+            Team01Driver.log("Database exists: Skipping table creation and population");
+        } else {
+            Team01Driver.log("Existing database not found: Creating one and loading data from CSV.");
+            createDatabase(dbPath.getAbsolutePath(), username, password);
+            populateDatabase();
+        }
 
-	}
+    }
 
-	private void createDatabase(String path) {
-		createConnection(path);
-		try {
-			BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+    private void createDatabase(String path, String username, String password) {
+        createConnection(path, username, password);
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
 
-			StringBuilder query = new StringBuilder();
-			String line = "";
-			while ((line = bufferedReader.readLine()) != null) {
-				if (line.startsWith("--")) {
-					continue; // skip sql comments
-				}
-				query.append(line);
+            StringBuilder query = new StringBuilder();
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.startsWith("--")) {
+                    continue; // skip sql comments
+                }
+                query.append(line);
 
-				// found a query, execute it
-				if (line.endsWith(";")) {
-					Statement stmt = conn.createStatement();
-					Team01Driver.log(query.toString().substring(0, query.indexOf("(")).toLowerCase());
-					stmt.execute(query.toString());
-					query = new StringBuilder();
-				}
-			}
+                // found a query, execute it
+                if (line.endsWith(";")) {
+                    Statement stmt = conn.createStatement();
+                    Team01Driver.log(query.toString().substring(0, query.indexOf("(")).toLowerCase());
+                    stmt.execute(query.toString());
+                    query = new StringBuilder();
+                }
+            }
 
-			bufferedReader.close();
-		} catch (SQLException | IOException e) {
-			e.printStackTrace();
-		}
-	}
+            bufferedReader.close();
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void populateDatabase() {
-		try {
-			CustomerTable.populateCustomerTableFromCSV(conn, "newCustomerData.csv");
-			List<Option> optionList = OptionTable.populateOptionTableFromCSV(conn, "CarOptions.csv");
-			ModelTable.populateModelTableFromCSV(conn, "Vehicles.csv");
-			List<Dealer> dealers = DealerTable.populateDealerTableFromCSV(conn, "DealershipData.csv");
-			List<Vehicle> vehicles = VehicleTable.populateVehicleTableFromCSV(optionList, conn, "Vehicles.csv", "VehicleOptions.csv");
-			DealerInventoryTable.addAllVehiclesToAllDealers(conn, vehicles, dealers);
-			SaleTable.populateSaleTable(conn, "smallVehicle.csv", "newCustomerData.csv", "CarOptions.csv");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+    private void populateDatabase() {
+        try {
+            CustomerTable.populateCustomerTableFromCSV(conn, "newCustomerData.csv");
+            List<Option> optionList = OptionTable.populateOptionTableFromCSV(conn, "CarOptions.csv");
+            ModelTable.populateModelTableFromCSV(conn, "Vehicles.csv");
+            List<Dealer> dealers = DealerTable.populateDealerTableFromCSV(conn, "DealershipData.csv");
+            List<Vehicle> vehicles = VehicleTable.populateVehicleTableFromCSV(optionList, conn, "Vehicles.csv", "VehicleOptions.csv");
+            DealerInventoryTable.addAllVehiclesToAllDealers(conn, vehicles, dealers);
+            SaleTable.populateSaleTable(conn, "smallVehicle.csv", "newCustomerData.csv", "CarOptions.csv");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * Create a database connection with the given params
-	 *  @param location : path of where to place the database
-	 * */
-	private void createConnection(String location) {
-		try {
-			Class.forName("org.h2.Driver");
-			conn = DriverManager.getConnection("jdbc:h2:file:" + location, "admin", "password");
-		} catch (SQLException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * Create a database connection with the given params
+     *
+     * @param location : path of where to place the database
+     * @param user:     username
+     * @param password: password
+     */
+    private void createConnection(String location, String user, String password) {
+        try {
+            Class.forName("org.h2.Driver");
+            conn = DriverManager.getConnection("jdbc:h2:file:" + location, user, password);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * When your database program exits you should close the connection
-	 */
-	public void closeConnection() {
-		try {
-			conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * When your database program exits you should close the connection
+     */
+    public void closeConnection() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public Connection getConnection() {
-		return this.conn;
-	}
+    public Connection getConnection() {
+        return this.conn;
+    }
 
-	public static void main(String[] args) {
-		DBConnector main = new DBConnector();
-		SaleTable.printSaleTable(main.getConnection());
-	}
+    public static void main(String[] args) {
+        DBConnector main = new DBConnector("admin", "password");
+    }
 }
--- src/main/Team01Driver.java
+++ src/main/Team01Driver.java
@@ -7,56 +7,57 @@ import java.util.Date;
 
 public class Team01Driver {
 
-	private static Team01Driver driver;
-	private DBConnector dbConnector;
-	private ViewManager viewManager;
-	private UserType userType;
+    private static Team01Driver driver;
+    private DBConnector dbConnector;
+    private ViewManager viewManager;
+    private UserType userType;
 
-	public static void main(String[] args) {
-		driver = new Team01Driver();
-		//TODO: Get DB setup and maybe load credentials from a config file?
-	}
+    public static void main(String[] args) {
+        String user = "admin";
+        String password = "password";
+        driver = new Team01Driver(user, password); // Optional: Create .config file and place username/password/other settings
+    }
 
-	/*
-	 * Singleton patterns for the driver
-	 */
-	public static Team01Driver getDriver() {
-		return driver;
-	}
+    /*
+     * Singleton patterns for the driver
+     */
+    public static Team01Driver getDriver() {
+        return driver;
+    }
 
-	public Team01Driver() {
-		dbConnector = new DBConnector();
+    public Team01Driver(String username, String password) {
+        dbConnector = new DBConnector(username, password);
 
-		SwingUtilities.invokeLater(() -> {
-			viewManager = new ViewManager();
-			viewManager.setupUserSelect();
-		});
+        SwingUtilities.invokeLater(() -> {
+            viewManager = new ViewManager();
+            viewManager.setupUserSelect();
+        });
 
-	}
+    }
 
-	public DBConnector getDB() {
-		return dbConnector;
-	}
+    public DBConnector getDB() {
+        return dbConnector;
+    }
 
-	public ViewManager getViewManager() {
-		return viewManager;
-	}
+    public ViewManager getViewManager() {
+        return viewManager;
+    }
 
-	public UserType getUserType() {
-		return userType;
-	}
+    public UserType getUserType() {
+        return userType;
+    }
 
-	public void setUserType(UserType userType) {
-		this.userType = userType;
-	}
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
 
-	public static void log(String message) {
-		String date = String.format("(INFO) [%1$tH:%1$tM:%1$tS] ", new Date());
-		System.out.println(date + message);
-	}
+    public static void log(String message) {
+        String date = String.format("(INFO) [%1$tH:%1$tM:%1$tS] ", new Date());
+        System.out.println(date + message);
+    }
 
-	public static void debug(String message) {
-		String date = String.format("(DEBUG) [%1$tH:%1$tM:%1$tS] ", new Date());
-		System.out.println(date + message);
-	}
+    public static void debug(String message) {
+        String date = String.format("(DEBUG) [%1$tH:%1$tM:%1$tS] ", new Date());
+        System.out.println(date + message);
+    }
 }
