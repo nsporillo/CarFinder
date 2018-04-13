d204ebadacf21046fc8baafd47ef3f8a424555ea
DavidIsraelChambers <dic9883@rit.edu>
2018-04-12 21:41:32 -0400

    Added checkboxes for Dealer searches

--- src/views/DealerSearchView.java
+++ src/views/DealerSearchView.java
@@ -11,7 +11,9 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DealerSearchView extends JFrame {
 
@@ -28,20 +30,26 @@ public class DealerSearchView extends JFrame {
 	private DealerSearch dealerSearch;
 
 	/**
+	 * Column Toggling Variables
+	 */
+	protected Map<String, Boolean> columnToggle = new LinkedHashMap<>(); // LinkedHashMap preserves insertion order
+	private List<Dealer> displayedDealers;
+
+	/**
 	 * Create the frame.
 	 */
 	public DealerSearchView() {
 		setResizable(false);
 		setTitle("Search Dealers");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 800, 500);
+		setBounds(100, 100, 800, 550);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 200, 324);
+		panel.setBounds(10, 11, 200, 314);
 		contentPane.add(panel);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
@@ -127,7 +135,7 @@ public class DealerSearchView extends JFrame {
 		 * Create scrollable result pane to be filled in with dealer results
 		 */
 		JScrollPane scrollPane = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setBounds(220, 11, 551, 400);
+		scrollPane.setBounds(220, 11, 551, 390);
 		contentPane.add(scrollPane);
 
 		resultPanel = new JPanel();
@@ -139,8 +147,8 @@ public class DealerSearchView extends JFrame {
 		searchButton.addActionListener(arg0 -> {
 			try {
 				dealerSearch = fromFields();
-				List<Dealer> results = dealerSearch.execute(Team01Driver.getDriver().getDB().getConnection());
-
+				//List<Dealer> results = dealerSearch.execute(Team01Driver.getDriver().getDB().getConnection());
+				displayedDealers = dealerSearch.execute(Team01Driver.getDriver().getDB().getConnection());
 
 				// Remove any possible previous results
 				resultPanel.removeAll();
@@ -150,11 +158,11 @@ public class DealerSearchView extends JFrame {
 				resultPanel.add(fromDealer(Dealer.label(), false));
 
 				// Display all vehicle results
-				for (Dealer d : results) {
+				for (Dealer d : displayedDealers) {
 					resultPanel.add(fromDealer(d, true));
 				}
 
-				resultPanel.revalidate();
+				resultPanel.updateUI();
 			} catch (Exception ex) {
 				ex.printStackTrace(System.err);
 			}
@@ -166,10 +174,104 @@ public class DealerSearchView extends JFrame {
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setBounds(10, 346, 200, 2);
 		contentPane.add(separator_1);
+
+
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setBounds(0, 464, 850, 2);
+		contentPane.add(separator_2);
+
+		JLabel lblNewLabel = new JLabel("Toggle Columns ");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setBounds(5, 468, 190, 52);
+		contentPane.add(lblNewLabel);
+
+		JCheckBox columnName = new JCheckBox("Name");
+		columnName.setSelected(true);
+		columnToggle.put("Name", true);
+		columnName.setBounds(300, 467, 100, 23);
+		columnName.addActionListener(new DealerSearchView.ColumnListener("Name"));
+		contentPane.add(columnName);
+
+		JCheckBox columnID = new JCheckBox("ID");
+		columnToggle.put("ID", false);
+		columnID.setBounds(300, 497, 100, 23);
+		columnID.addActionListener(new DealerSearchView.ColumnListener("ID"));
+		contentPane.add(columnID);
+
+		JCheckBox columnStreet = new JCheckBox("Street");
+		columnStreet.setSelected(true);
+		columnToggle.put("Street", true);
+		columnStreet.setBounds(200, 497, 100, 23);
+		columnStreet.addActionListener(new DealerSearchView.ColumnListener("Street"));
+		contentPane.add(columnStreet);
+
+		JCheckBox columnCity = new JCheckBox("City");
+		columnCity.setSelected(true);
+		columnToggle.put("City", true);
+		columnCity.addActionListener(new DealerSearchView.ColumnListener("City"));
+		columnCity.setBounds(500, 467, 100, 23);
+		contentPane.add(columnCity);
+
+		JCheckBox columnState = new JCheckBox("State");
+		columnState.setSelected(true);
+		columnToggle.put("State", true);
+		columnState.setBounds(400, 467, 100, 23);
+		columnState.addActionListener(new DealerSearchView.ColumnListener("State"));
+		contentPane.add(columnState);
+
+		JCheckBox columnZip = new JCheckBox("Zip");
+		columnZip.setSelected(true);
+		columnZip.setBounds(400, 497, 100, 23);
+		columnToggle.put("Zip", true);
+		columnZip.addActionListener(new DealerSearchView.ColumnListener("Zip"));
+		contentPane.add(columnZip);
+
+		JCheckBox columnPhone = new JCheckBox("Phone");
+		columnPhone.setSelected(true);
+		columnPhone.setBounds(500, 497, 150, 23);
+		columnToggle.put("Phone", true);
+		columnPhone.addActionListener(new DealerSearchView.ColumnListener("Phone"));
+		contentPane.add(columnPhone);
+
+	}
+
+	protected void renderResults() {
+		if (displayedDealers != null) {
+			// Remove any possible previous results
+			resultPanel.removeAll();
+			resultPanel.validate();
+
+			// Add header
+			resultPanel.add(fromDealer(Dealer.label(), false));
+
+			// Display all vehicle results
+			for (Dealer d : displayedDealers) {
+				resultPanel.add(fromDealer(d, true));
+			}
+
+			resultPanel.updateUI();
+		}
+	}
+
+	class ColumnListener implements ActionListener {
+
+		private String column;
+
+		ColumnListener(String column) {
+			this.column = column;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Boolean oldValue = columnToggle.get(column);
+			columnToggle.put(column, !oldValue);
+			SwingUtilities.invokeLater(DealerSearchView.this::renderResults);
+		}
 	}
 
 	private JButton fromDealer(final Dealer dealer, boolean clickable) {
-		JButton jButton = new JButton(dealer.getSearchView());
+		JButton jButton = new JButton(dealer.getSearchView(columnToggle));
 
 		if (clickable) {
 			jButton.addActionListener(e -> {
@@ -226,4 +328,6 @@ public class DealerSearchView extends JFrame {
 		// TODO: Load dealer view for the specified dealer
 
 	}
+
+
 }
